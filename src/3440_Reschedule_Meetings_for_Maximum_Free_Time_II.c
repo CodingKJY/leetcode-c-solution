669962d3320536e07274ad15f4df5e116cf84bc3
james.yang <james.yang@gtbooster.com>
2025-07-30 19:15:36 +0800

    update 3440

--- src/3440_Reschedule_Meetings_for_Maximum_Free_Time_II.c
+++ src/3440_Reschedule_Meetings_for_Maximum_Free_Time_II.c
@@ -5,88 +5,60 @@
 // Implementation
 // 
 int maxFreeTime(int eventTime, int* startTime, int startTimeSize, int* endTime, int endTimeSize) {
-    return 0;
+    int maxFreeTime = 0;
+    int left_gap, main_gap, right_gap;
+    int _left_gap, _main_gap, _right_gap;
+    int size = startTimeSize;
+
+    left_gap = startTime[0];
+    main_gap = startTime[1] - endTime[0];
+    right_gap = size == 2 ? eventTime - endTime[size - 1] : startTime[2] - endTime[1];
+    for (int i = 2; i < size; i += 2) {
+        
+    }
+
+    return maxFreeTime;
 }
 
 // 
 // Run Test
 // 
 typedef struct {
-    int start;
-    int end;
-} Event;
-#define EVENT(start, end) (Event){start,end}
-#define MAX_EVENTS 100000
-
-typedef struct {
     int eventTime;
-    int n;
-    Event events[MAX_EVENTS];
-} Events_Series;
-
-typedef struct {
-    int eventTime;
-    int n;
-    int *startTime;
-    int *endTime;
+    int eventSize;
+    int startTime[100000];
+    int endTime[100000];
 } Testcase;
 
 void solution(void) {
-    Events_Series series[] = {
+    Testcase tests[] = {
         {  // 2
-            .n = 2,
-            .events = {
-                EVENT(1,2),
-                EVENT(3,5),
-            },
             .eventTime = 5,
+            .eventSize = 2,
+            .startTime = {1,3},
+            .endTime   = {2,5},
         },
         {  // 7
-            .n = 3,
-            .events = {
-                EVENT(0,1),
-                EVENT(7,8),
-                EVENT(9,10),
-            },
             .eventTime = 10,
+            .eventSize = 3,
+            .startTime = {0,7,9},
+            .endTime   = {1,8,10},
         },
         {  // 6
-            .n = 4,
-            .events = {
-                EVENT(0,1),
-                EVENT(3,4),
-                EVENT(7,8),
-                EVENT(9,10),
-            },
             .eventTime = 10,
+            .eventSize = 4,
+            .startTime = {0,3,7,9},
+            .endTime   = {1,4,8,10},
         },
         {  // 0
-            .n = 5,
-            .events = {
-                EVENT(0,1),
-                EVENT(1,2),
-                EVENT(2,3),
-                EVENT(3,4),
-                EVENT(4,5),
-            },
             .eventTime = 5,
+            .eventSize = 5,
+            .startTime = {0,1,2,3,4},
+            .endTime   = {1,2,3,4,5},
         },
     };
 
-    for (int i = 0; i < ARRAY_SIZE(series); i++) {
-        Testcase test;
-        test.n = series[i].n;
-        test.eventTime = series[i].eventTime;
-        test.startTime = malloc(test.n * sizeof(int));
-        test.endTime = malloc(test.n * sizeof(int));
-        for (int j = 0; j < test.n; j++) {
-            test.startTime[j] = series[i].events[j].start;
-            test.endTime[j] = series[i].events[j].end;
-        }
-        printf("%d\n", maxFreeTime(test.eventTime, test.startTime, test.n, test.endTime, test.n));
-        if (test.startTime) free(test.startTime);
-        if (test.endTime) free(test.endTime);
-        test.startTime = NULL;
-        test.endTime = NULL;
+    for (int i = 0; i < ARRAY_SIZE(tests); i++) {
+        printf("%d\n", maxFreeTime(tests[i].eventTime, tests[i].startTime, tests[i].eventSize, tests[i].endTime, tests[i].eventSize));
     }
 }
