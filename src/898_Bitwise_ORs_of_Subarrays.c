ba7ebc6144fd4a009be41134f05794ca748d0494
james.yang <james.yang@gtbooster.com>
2025-08-01 18:39:47 +0800

    update 898 solution

--- src/898_Bitwise_ORs_of_Subarrays.c
+++ src/898_Bitwise_ORs_of_Subarrays.c
@@ -1,15 +1,65 @@
-#define SOLVED 0
+#define SOLVED 1
 #include "leetcode.h"
 
 // 
 // Implementation
 // 
+typedef struct {
+    int val;
+    UT_hash_handle hh;
+} HINT32;
+
+typedef struct {
+    HINT32 *elts;
+    int counts;
+} Set;
+
+void setAdd(Set *set, int val) {
+    HINT32 *elt = NULL;
+    HASH_FIND(hh, set->elts, &val, sizeof(int), elt);
+    if (!elt) {
+        elt = malloc(sizeof(HINT32));
+        elt->val = val;
+        HASH_ADD_KEYPTR(hh, set->elts, &(elt->val), sizeof(int), elt);
+        set->counts++;
+    }
+}
+
+void setFree(Set *set) {
+    HINT32 *elt, *tmp;
+    HASH_ITER(hh, set->elts, elt, tmp) {
+        HASH_DELETE(hh, set->elts, elt);
+        free(elt);
+    }
+}
+
 int subarrayBitwiseORs(int* arr, int arrSize) {
-    int subArrNum = 0;
+    int counts;
+    Set set = {0};
+    Set prev = {0};
 
-
+    for (int i = 0; i < arrSize; i++) {
+        Set cur = {0};
+        HINT32 *elt, *tmp;
     
-    return subArrNum;
+        setAdd(&cur, arr[i]);
+        HASH_ITER(hh, prev.elts, elt, tmp) {
+            int val = arr[i] | elt->val;
+            setAdd(&cur, val);
+        }
+
+        HASH_ITER(hh, cur.elts, elt, tmp) {
+            setAdd(&set, elt->val);
+        }
+
+        setFree(&prev);
+        prev.elts = cur.elts;
+        prev.counts = cur.counts;
+    }
+    counts = set.counts;
+    setFree(&prev);
+    setFree(&set);
+    return counts;
 }
 
 // 
